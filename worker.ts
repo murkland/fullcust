ab0ee45dd873cf9c0d05e448af10f08d89d277fe
bigfarts <bigfarts@punymail.com>
2023-06-18 20:33:23 -0700

    Revert "combine worker and async-solver"
    This reverts commit 564612e3a870a1b5a0d094bf20449261dd06f93c.

---
+++ worker.ts
@@ -0,0 +1,39 @@
+import { GridSettings, Part, Requirement, Solution, solve } from "./solver";
+
+let it: Iterator<Solution> | null = null;
+
+self.onmessage = function (
+    e: MessageEvent<
+        | { type: "next" }
+        | {
+              type: "init";
+              args: {
+                  parts: Part[];
+                  requirements: Requirement[];
+                  gridSettings: GridSettings;
+                  spinnableColors: boolean[];
+              };
+          }
+    >
+) {
+    console.time(e.data.type);
+    switch (e.data.type) {
+        case "init": {
+            const { parts, requirements, gridSettings, spinnableColors } =
+                e.data.args;
+            it = solve(parts, requirements, gridSettings, spinnableColors)[
+                Symbol.iterator
+            ]();
+            break;
+        }
+
+        case "next": {
+            const r = it!.next();
+            self.postMessage({ type: "next", ...r });
+            break;
+        }
+    }
+    console.timeEnd(e.data.type);
+};
+
+self.postMessage({ type: "ready" });
