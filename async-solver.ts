564612e3a870a1b5a0d094bf20449261dd06f93c
bigfarts <bigfarts@punymail.com>
2023-06-18 20:32:01 -0700

    combine worker and async-solver

--- async-solver.ts
+++ async-solver.ts
@@ -1,4 +1,4 @@
-import { GridSettings, Part, Requirement, Solution } from "./solver";
+import { GridSettings, Part, Requirement, Solution, solve } from "./solver";
 
 export default class AsyncSolver {
     worker: Worker;
@@ -10,9 +10,12 @@ export default class AsyncSolver {
         gridSettings: GridSettings,
         spinnableColors: boolean[]
     ) {
-        const worker = new Worker(new URL("./worker.ts", import.meta.url), {
-            type: "module",
-        });
+        const worker = new Worker(
+            new URL("./async-solver.ts", import.meta.url),
+            {
+                type: "module",
+            }
+        );
         this.worker = worker;
         const args = { parts, requirements, gridSettings, spinnableColors };
 
@@ -62,4 +65,48 @@ export default class AsyncSolver {
     kill() {
         this.worker.terminate();
     }
+}
+
+declare var WorkerGlobalScope: any;
+if (
+    typeof WorkerGlobalScope !== "undefined" &&
+    self instanceof WorkerGlobalScope
+) {
+    let it: Iterator<Solution> | null = null;
+
+    self.onmessage = function (
+        e: MessageEvent<
+            | { type: "next" }
+            | {
+                  type: "init";
+                  args: {
+                      parts: Part[];
+                      requirements: Requirement[];
+                      gridSettings: GridSettings;
+                      spinnableColors: boolean[];
+                  };
+              }
+        >
+    ) {
+        console.time(e.data.type);
+        switch (e.data.type) {
+            case "init": {
+                const { parts, requirements, gridSettings, spinnableColors } =
+                    e.data.args;
+                it = solve(parts, requirements, gridSettings, spinnableColors)[
+                    Symbol.iterator
+                ]();
+                break;
+            }
+
+            case "next": {
+                const r = it!.next();
+                self.postMessage({ type: "next", ...r });
+                break;
+            }
+        }
+        console.timeEnd(e.data.type);
+    };
+
+    self.postMessage({ type: "ready" });
 }
